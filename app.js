8adcaa49d7e888ab3cf1762a08b0a69b65c00422
Leigh <leighpri@gmail.com>
2015-09-28 18:22:06 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+//OK new plan, make the funcitons that generate the data first then worry about customizing numbers and types.
+
+//populate a big object that has some data in it to play with (will eventually be changed to a JSON file to manage separately)
+
+//this is acceptable for JSON except the keys should be in quotes too
+var dataPool = {
+  firstName: ["Alison", "Bob", "Susan", "Anthony", "Samantha", "Daniel", "Jack", "Leigh", "Brian", "Christopher", "Steven", "Albert"],
+  lastName: ["Stevenson", "Johnson", "Cox", "Smith", "Cooke", "Carter", "Jackson", "Akers", "Stiles", "Anderson", "Addams", "Samuels"],
+  job: ["teacher", "dentist", "web developmer", "artist", "internet troll", "writer", "mathmetician", "project manager"]
+};
+
+//defines RNG generator for this app
+function getRNG(min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
+
+//generate and stores a random number to determine how many objects will be created (max 10 for now)
+$("#generateButton").click(function(){
+var totalUsers = getRNG(1,11);
+var outputHolder = ""; //resets output in case user generates a new set of data
+var tempObject = "";
+
+$.each(dataPool, function(key, value) {
+    tempObject += '"' + key + '": '
+    var tempNum = getRNG(0, value.length);
+    tempObject += '"' + value[tempNum] + '",';
+});
+tempObject.slice(0,-1); //removes last ,
+console.log(tempObject);
+
+//constructor function...might not use
+// function User(firstName, lastName, email, job){
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.email = email;
+//   this.job = job;
+// }
+
+//create a function that loops through an object, generating a random number to select an option from each array
+
+//iterates over whole object, selecting a random value from each array
+
+
+
+//push results onto temp array
+//display completed temp array in <textarea>
+
+
+});
+
+
+//idea for later customization:
+  //generate email address based on firstname + lastname and a stock of domains
+    //still later, allow user to input own @whatever.com e-mail field
+  //allow option of a string with a full name or separate first/last names
+
+  // var numUsersTemp = $("#userNum").keyup(function(){
+  //   return this.value();
+  // });
+  //
+  // var numUsers = parseInt(numUsersTemp);
+  //
+  // $("#generateButton").click(function(){
+  //   console.log(numUsers);
+  // });
